3bdeaaeeff5c3c91f315a40d6e0948622dc5de8a
Chris Neale <Chris.Neale@opengi.co.uk>
2018-11-07 20:42:34 +0000

    NetDev: change folder structure

---
+++ Ogi.InterviewChallenges.Tests/General/AdventOfCodeTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ogi.InterviewChallenges.General;
+
+namespace Ogi.InterviewChallenges.Tests.General
+{
+    public class AdventOfCodeTests
+    {
+
+        [DataTestMethod]
+        [DataRow("1122", 3)]
+        [DataRow("1111", 4)]
+        [DataRow("1234", 0)]
+        [DataRow("91212129", 9)]
+        public void InverseCaptchaTest(string key, int expectedResult)
+        {
+                Assert.AreEqual(expectedResult, AdventOfCode.InverseCaptcha(key));
+        }
+
+        [DataTestMethod]
+        [DataRow("1212", 0.5, 6)]
+        [DataRow("1221", 0.5, 0)]
+        [DataRow("123425", 0.5, 4)]
+        [DataRow("123123", 0.5, 12)]
+        [DataRow("12131415", 0.5, 4)]
+        public void InverseCaptchaWithLookaheadFactorTest(string key, double lookAheadFactor, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, AdventOfCode.InverseCaptcha(key, lookAheadFactor));
+        }
+
+    }
+}
--- Ogi.InterviewChallenges/General/AdventOfCode.cs
+++ Ogi.InterviewChallenges/General/AdventOfCode.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 
-namespace Ogi.InterviewChallenges.NetDev
+namespace Ogi.InterviewChallenges.General
 {
-    public static partial class StaticTestMethods
+    public static class AdventOfCode
     {      
         //from https://adventofcode.com/2017/day/1
         //in progress
